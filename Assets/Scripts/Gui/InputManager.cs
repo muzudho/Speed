264798b32a534a248753c36a62a045d8bfa9b200
むずでょ <muzudho1@gmail.com>
2023-02-07 08:13:05 +0900

    Spam 対策

--- Assets/Scripts/Gui/InputManager.cs
+++ Assets/Scripts/Gui/InputManager.cs
@@ -1,4 +1,3 @@
-using Assets.Scripts.Gui.SpanOfLerp;
 using Assets.Scripts.Gui.SpanOfLerp.TimedGenerator;
 using Assets.Scripts.ThinkingEngine;
 using Assets.Scripts.ThinkingEngine.CommandArgs;
@@ -10,6 +9,8 @@ public class InputManager : MonoBehaviour
     // - フィールド
 
     ScheduleRegister scheduleRegister;
+
+    float[] spamSeconds = new[] { 0f, 0f };
 
     // - イベントハンドラ
 
@@ -27,110 +28,171 @@ public class InputManager : MonoBehaviour
     /// </summary>
     void Update()
     {
+        // もう入力できないなら真
+        bool[] handled = { false, false };
+
+        for (var player = 0; player < 2; player++)
+        {
+            // 前判定
+            // もう入力できないなら真
+            handled[player] = 0 < spamSeconds[player];
+
+            // スパン時間消化
+            if (0 < spamSeconds[player])
+            {
+                // 負数になっても気にしない
+                spamSeconds[player] -= Time.deltaTime;
+            }
+        }
+
         const int right = 0;// 台札の右
         const int left = 1;// 台札の左
-        bool handled1player = false;
-        bool handled2player = false;
 
         // 先に登録したコマンドの方が早く実行される
 
         // （ボタン押下が同時なら）右の台札は１プレイヤー優先
         // ==================================================
 
-        if (Input.GetKeyDown(KeyCode.DownArrow) && LegalMove.CanPutToCenterStack(
-            gameModel: scheduleRegister.GameModel,
-            player: 0,      // １プレイヤーが
-            place: right))  // 右の
+        // １プレイヤー
         {
-            // １プレイヤーが、ピックアップ中の場札を抜いて、（１プレイヤーから見て）右の台札へ積み上げる
-            scheduleRegister.AddJustNow(new GuiOfTimedCommandArgs.Model(new MoveCardToCenterStackFromHandModel(
-                player: 0,      // １プレイヤーが
-                place: right))); // 右の
-            handled1player = true;
+            var player = 0;
+            if (!handled[player] && Input.GetKeyDown(KeyCode.DownArrow) && LegalMove.CanPutToCenterStack(
+                gameModel: scheduleRegister.GameModel,
+                player: player,
+                place: right))  // 右の
+            {
+                // １プレイヤーが、ピックアップ中の場札を抜いて、（１プレイヤーから見て）右の台札へ積み上げる
+                var timedCommandArg = new GuiOfTimedCommandArgs.Model(new MoveCardToCenterStackFromHandModel(
+                    player: player,      // １プレイヤーが
+                    place: right)); // 右の
+
+                spamSeconds[player] = timedCommandArg.Duration;
+                scheduleRegister.AddJustNow(timedCommandArg);
+                handled[player] = true;
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.W) && LegalMove.CanPutToCenterStack(
-            gameModel: scheduleRegister.GameModel,
-            player: 1,      // ２プレイヤーが
-            place: right))  // 右の)
+        // ２プレイヤー
         {
-            // ２プレイヤーが、ピックアップ中の場札を抜いて、（１プレイヤーから見て）右の台札へ積み上げる
-            scheduleRegister.AddJustNow(new GuiOfTimedCommandArgs.Model(new MoveCardToCenterStackFromHandModel(
-                player: 1, // ２プレイヤーが
-                place: right))); // 右の
-            handled2player = true;
+            var player = 1;
+            if (!handled[player] && Input.GetKeyDown(KeyCode.W) && LegalMove.CanPutToCenterStack(
+                gameModel: scheduleRegister.GameModel,
+                player: player,
+                place: right))  // 右の)
+            {
+                // ２プレイヤーが、ピックアップ中の場札を抜いて、（１プレイヤーから見て）右の台札へ積み上げる
+                var timedCommandArg = new GuiOfTimedCommandArgs.Model(new MoveCardToCenterStackFromHandModel(
+                    player: player,      // ２プレイヤーが
+                    place: right)); // 右の
+
+                spamSeconds[player] = timedCommandArg.Duration;
+                scheduleRegister.AddJustNow(timedCommandArg);
+                handled[player] = true;
+            }
         }
 
         // （ボタン押下が同時なら）左の台札は２プレイヤー優先
         // ==================================================
 
         // ２プレイヤー
-        if (Input.GetKeyDown(KeyCode.S) && LegalMove.CanPutToCenterStack(
-            gameModel: scheduleRegister.GameModel,
-            player: 1,      // ２プレイヤーが
-            place: left))
         {
-            // ２プレイヤーが、ピックアップ中の場札を抜いて、（１プレイヤーから見て）左の台札へ積み上げる
-            scheduleRegister.AddJustNow(new GuiOfTimedCommandArgs.Model(new MoveCardToCenterStackFromHandModel(
-                player: 1,      // ２プレイヤーが
-                place: left)));  // 左の
-            handled2player = true;
+            var player = 1;
+            if (!handled[player] && Input.GetKeyDown(KeyCode.S) && LegalMove.CanPutToCenterStack(
+                gameModel: scheduleRegister.GameModel,
+                player: player,
+                place: left))
+            {
+                // ２プレイヤーが、ピックアップ中の場札を抜いて、（１プレイヤーから見て）左の台札へ積み上げる
+                var timedCommandArg = new GuiOfTimedCommandArgs.Model(new MoveCardToCenterStackFromHandModel(
+                    player: player,      // ２プレイヤーが
+                    place: left));  // 左の
+
+                spamSeconds[player] = timedCommandArg.Duration;
+                scheduleRegister.AddJustNow(timedCommandArg);
+                handled[player] = true;
+            }
         }
 
         // １プレイヤー
-        if (Input.GetKeyDown(KeyCode.UpArrow) && LegalMove.CanPutToCenterStack(
-            gameModel: scheduleRegister.GameModel,
-            player: 0,      // １プレイヤーが
-            place: left))
         {
-            // １プレイヤーが、ピックアップ中の場札を抜いて、（１プレイヤーから見て）左の台札へ積み上げる
-            scheduleRegister.AddJustNow(new GuiOfTimedCommandArgs.Model(new MoveCardToCenterStackFromHandModel(
-                player: 0, // １プレイヤーが
-                place: left)));  // 左の
-            handled1player = true;
+            var player = 0;
+            if (!handled[player] && Input.GetKeyDown(KeyCode.UpArrow) && LegalMove.CanPutToCenterStack(
+                gameModel: scheduleRegister.GameModel,
+                player: player,
+                place: left))
+            {
+                // １プレイヤーが、ピックアップ中の場札を抜いて、（１プレイヤーから見て）左の台札へ積み上げる
+                var timedCommandArg = new GuiOfTimedCommandArgs.Model(new MoveCardToCenterStackFromHandModel(
+                    player: player,      // １プレイヤーが
+                    place: left));  // 左の
+
+                spamSeconds[player] = timedCommandArg.Duration;
+                scheduleRegister.AddJustNow(timedCommandArg);
+                handled[player] = true;
+            }
         }
 
         // それ以外のキー入力は、同時でも勝敗に関係しない
         // ==============================================
 
         // １プレイヤー
-        if (handled1player)
         {
+            var player = 0;
 
-        }
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            // １プレイヤーのピックアップしているカードから見て、（１プレイヤーから見て）左隣のカードをピックアップするように変えます
-            scheduleRegister.AddJustNow(new GuiOfTimedCommandArgs.Model(new MoveFocusToNextCardModel(
-                player: 0,
-                direction: 1)));
-        }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            // １プレイヤーのピックアップしているカードから見て、（１プレイヤーから見て）右隣のカードをピックアップするように変えます
-            scheduleRegister.AddJustNow(new GuiOfTimedCommandArgs.Model(new MoveFocusToNextCardModel(
-                player: 0,
-                direction: 0)));
+            if (handled[player])
+            {
+
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                // １プレイヤーのピックアップしているカードから見て、（１プレイヤーから見て）左隣のカードをピックアップするように変えます
+                var timedCommandArg = new GuiOfTimedCommandArgs.Model(new MoveFocusToNextCardModel(
+                    player: player,
+                    direction: 1));
+
+                spamSeconds[player] = timedCommandArg.Duration;
+                scheduleRegister.AddJustNow(timedCommandArg);
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                // １プレイヤーのピックアップしているカードから見て、（１プレイヤーから見て）右隣のカードをピックアップするように変えます
+                var timedCommandArg = new GuiOfTimedCommandArgs.Model(new MoveFocusToNextCardModel(
+                    player: player,
+                    direction: 0));
+
+                spamSeconds[player] = timedCommandArg.Duration;
+                scheduleRegister.AddJustNow(timedCommandArg);
+            }
         }
 
         // ２プレイヤー
-        if (handled2player)
         {
+            var player = 1;
 
-        }
-        else if (Input.GetKeyDown(KeyCode.A))
-        {
-            // ２プレイヤーのピックアップしているカードから見て、（２プレイヤーから見て）左隣のカードをピックアップするように変えます
-            scheduleRegister.AddJustNow(new GuiOfTimedCommandArgs.Model(new MoveFocusToNextCardModel(
-                player: 1,
-                direction: 1)));
-        }
-        else if (Input.GetKeyDown(KeyCode.D))
-        {
-            // ２プレイヤーのピックアップしているカードから見て、（２プレイヤーから見て）右隣のカードをピックアップするように変えます
-            scheduleRegister.AddJustNow(new GuiOfTimedCommandArgs.Model(new MoveFocusToNextCardModel(
-                player: 1,
-                direction: 0)));
+            if (handled[player])
+            {
+
+            }
+            else if (Input.GetKeyDown(KeyCode.A))
+            {
+                // ２プレイヤーのピックアップしているカードから見て、（２プレイヤーから見て）左隣のカードをピックアップするように変えます
+                var timedCommandArg = new GuiOfTimedCommandArgs.Model(new MoveFocusToNextCardModel(
+                    player: player,
+                    direction: 1));
+
+                spamSeconds[player] = timedCommandArg.Duration;
+                scheduleRegister.AddJustNow(timedCommandArg);
+            }
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                // ２プレイヤーのピックアップしているカードから見て、（２プレイヤーから見て）右隣のカードをピックアップするように変えます
+                var timedCommandArg = new GuiOfTimedCommandArgs.Model(new MoveFocusToNextCardModel(
+                    player: player,
+                    direction: 0));
+
+                spamSeconds[player] = timedCommandArg.Duration;
+                scheduleRegister.AddJustNow(timedCommandArg);
+            }
         }
 
         // デバッグ用
@@ -140,9 +202,12 @@ public class InputManager : MonoBehaviour
             for (var player = 0; player < 2; player++)
             {
                 // 場札を並べる
-                scheduleRegister.AddJustNow(new GuiOfTimedCommandArgs.Model(new MoveCardsToHandFromPileModel(
+                var timedCommandArg = new GuiOfTimedCommandArgs.Model(new MoveCardsToHandFromPileModel(
                     player: player,
-                    numberOfCards: 1)));
+                    numberOfCards: 1));
+
+                spamSeconds[player] = timedCommandArg.Duration;
+                scheduleRegister.AddJustNow(timedCommandArg);
             }
         }
     }
