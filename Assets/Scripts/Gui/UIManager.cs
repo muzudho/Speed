40dc80a91b1403f5720a8b593ad6b25df1a2dee1
むずでょ <muzudho1@gmail.com>
2023-02-12 19:18:55 +0900

    UIs

--- Assets/Scripts/Gui/UIManager.cs
+++ Assets/Scripts/Gui/UIManager.cs
@@ -6,6 +6,9 @@ public class UIManager : MonoBehaviour
     // フィールド
 
     [SerializeField] GameObject playerSelectBackground;
+    [SerializeField] GameObject playerButtons;
+    [SerializeField] GameObject p1Keys;
+    [SerializeField] GameObject p2Keys;
 
     InputManager inputManager;
 
@@ -15,24 +18,32 @@ public class UIManager : MonoBehaviour
     {
         inputManager.Computers = new Computer[] { null, null };
         playerSelectBackground.SetActive(false);
+        playerButtons.SetActive(false);
+        p1Keys.SetActive(true);
+        p2Keys.SetActive(true);
     }
 
     public void On1pVsCom()
     {
         inputManager.Computers = new Computer[] { null, new Computer(1) };
         playerSelectBackground.SetActive(false);
+        playerButtons.SetActive(false);
+        p1Keys.SetActive(true);
     }
 
     public void OnComVs2p()
     {
         inputManager.Computers = new Computer[] { new Computer(0), null };
         playerSelectBackground.SetActive(false);
+        playerButtons.SetActive(false);
+        p2Keys.SetActive(true);
     }
 
     public void OnComVsCom()
     {
         inputManager.Computers = new Computer[] { new Computer(0), new Computer(1) };
         playerSelectBackground.SetActive(false);
+        playerButtons.SetActive(false);
     }
 
     // - イベントハンドラ
@@ -41,6 +52,9 @@ public class UIManager : MonoBehaviour
     void Start()
     {
         inputManager = GameObject.Find("Input Manager").GetComponent<InputManager>();
+
+        p1Keys.SetActive(false);
+        p2Keys.SetActive(false);
     }
 
     // Update is called once per frame
